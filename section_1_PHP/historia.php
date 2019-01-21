9456a2a4fc2118f72b48fa68e9f4b81b7b0cd3ff
ThiagoMLima25 <thiagolima0993@gmail.com>
2019-01-20 23:50:33 -0200

    correção de arquivos

--- section_1_PHP/historia.php
+++ section_1_PHP/historia.php
@@ -1,21 +1,32 @@
 <?php 
 	/**
-	 * RASMUS LERDORF - Criador do PHP
+	 * RASMUS LERDORF Foi o criador do PHP em 1994
 	 * Criou o php para ser ser um contador de seu site pessoal.
-	 * Ele o chamava de pessoal home page.
+	 * Ele o chamava de personal home page.
 	 * 
 	 * ANDI GUTMANS e ZEEV SURASKI são programadores israelenses, juntando ambos nomes forma-se
-	 * ZENDI que é a empresa que cuida do PHP e foi responsável por reescrever o core do PHP.
+	 * ZEND Technologies é a empresa que cuida do PHP e foi responsável por reescrever o core do PHP.
+	 * 
+	 * Quando instalamos o Apache como servidor web as portas TCP/IP - 80/443 ficam reservadas por
+	 * padrão para comunicação web.
+	 * 
+	 * O servidor de banco de dados MySQL quando instalado, ocupa por padrão a porta 3306,Firewall TCP/IP
 	 * 
 	 * Em 2015 foi lançado o PHP 7
 	 * 
 	 * O arquivo de configuração do APACHE é o HTTPD.conf 
 	 * O arquivo de configuração do PHP é o PHP.ini 
 	 * 
+	 * A linguagem de programação PHP é considerada uma linguagem fracamente tipada.
+	 * 
 	 * IDE -> Ambiente de desenvolvimento.
 	 * Editor de texto -> terá alguns recursos a mais do que os simples editores de texto.
-		<?php -> tag de abertura
-		
+	 * <?php -> tag de abertura
+	 * 
+	 * Ambiente Cliente/Servidor
+	 * 
+	 * O cliente faz uma requisição (request) ao servidor PHP. Que responde (response) sempre com HTML.
+	 * É necessário instalar no servidor o Apache e PHP manualmente. 
 	 */
 
 	echo "Meu primeiro código PHP! <strong> Olá Mundo :)</strong>";
---
+++ section_2_PHP/git.php
@@ -0,0 +1,6 @@
+<?php 
+	/**
+	 * O GIT Permite trabalhar de forma mais organizada, fazendo o controle de versão de seus projetos.
+	 * GitHub - Uma rede social de códigos "Welcome home developers" "work better togheter"
+	 */
+ ?>
