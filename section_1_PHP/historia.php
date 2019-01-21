5fffa0b2ff48bd9d81f69719aad4e88e60010d7e
ThiagoMLima25 <thiagolima0993@gmail.com>
2019-01-20 23:10:14 -0200

    Inicio do curso PHP 7

---
+++ section_1_PHP/historia.php
@@ -0,0 +1,22 @@
+<?php 
+	/**
+	 * RASMUS LERDORF - Criador do PHP
+	 * Criou o php para ser ser um contador de seu site pessoal.
+	 * Ele o chamava de pessoal home page.
+	 * 
+	 * ANDI GUTMANS e ZEEV SURASKI são programadores israelenses, juntando ambos nomes forma-se
+	 * ZENDI que é a empresa que cuida do PHP e foi responsável por reescrever o core do PHP.
+	 * 
+	 * Em 2015 foi lançado o PHP 7
+	 * 
+	 * O arquivo de configuração do APACHE é o HTTPD.conf 
+	 * O arquivo de configuração do PHP é o PHP.ini 
+	 * 
+	 * IDE -> Ambiente de desenvolvimento.
+	 * Editor de texto -> terá alguns recursos a mais do que os simples editores de texto.
+		<?php -> tag de abertura
+		
+	 */
+
+	echo "Meu primeiro código PHP! <strong> Hello World </strong>";
+ ?>
